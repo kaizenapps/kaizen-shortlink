83623be8e9f4a3a57db08283d9f011eefa80bba1
Preston Zen <36439263+prestonzen@users.noreply.github.com>
2024-11-06 02:14:28 +0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Kaizen Shortlink',
-  email: 'contact@kaizenapps.com',
-  twitter: 'https://x.com/kaizen_apps',
-  github: 'https://github.com/kaizenapps',
-  telegram: 'https://t.me/kaizenapps',
-  mastodon: 'https://mastodon.social/@kaizenapps',
-  blog: 'https://kaizenapps.com/blog/',
+  email: 'sink.cool@miantiao.me',
+  twitter: 'https://sink.cool/kai',
+  github: 'https://sink.cool/github',
+  telegram: 'https://sink.cool/telegram',
+  mastodon: 'https://sink.cool/mastodon',
+  blog: 'https://sink.cool/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
